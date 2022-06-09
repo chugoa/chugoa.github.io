cad0acbd0ff66a9643b176932cea2395cfb18d00
Cameron <cameron.h.altenkirch@gmail.com>
2022-06-09 18:51:12 +0100

    added JS

---
+++ app.js
@@ -0,0 +1,9 @@
+(function() {
+    var loginButton = document.getElementById('btn-login');
+
+    function test() {
+        console.log("TEST!!");
+    }
+    loginButton.addEventListener('click', test)
+})
+
